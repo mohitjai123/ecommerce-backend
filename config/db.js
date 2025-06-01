88eaf7372381afaf460cc1eadff1fc2a0db06cca
Mohit Jaiswal <103012169+mohitjai123@users.noreply.github.com>
2025-06-02 01:08:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('MongoDB connected testing');
+    console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
