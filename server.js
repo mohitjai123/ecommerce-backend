798d9de34cd220885c1447fd242fe285f24c9386
Mohit Jaiswal <103012169+mohitjai123@users.noreply.github.com>
2025-06-02 01:45:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors")
-
+//new comment
 const indexRoutes = require("./routes/index.routes")
 
 app.use(express.json());
